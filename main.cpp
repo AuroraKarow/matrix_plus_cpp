c11390272ca39d5168f9831753a7b2650186bbd0
AuroraKarow <karowmisaki@outlook.com>
2022-07-20 23:35:01 +0800

    Update demo

--- main.cpp
+++ main.cpp
@@ -10,25 +10,25 @@ using neunet::matrix::vect;
 
 int main(int argc, char *argv[], char *envp[])
 {
-    cout << "hello, world." << endl;
-    vect a = {{0},
-              {1},
-              {2},
-              {3}},
-         b = {{0, 1, 2},
-              {3, 4, 5},
-              {6, 7, 8}},
-         c = {{0, 1},
-              {2, 3}},
-         d = {{0, 1}},
-         e = {{0, 1, 2, 3}},
-         f = {{0}},
-         g = {{0, 1, 2}},
-         h = {{0, 1, 2}};
-    vect i = {{a, {{b, {{c},
-                        {d}}},
-                   {e,   f}}},
-              {g,        h}};
-    cout << i << endl;
-    return EXIT_SUCCESS;
+     cout << "hello, world." << endl;
+     vect a = {{0},
+               {1},
+               {2},
+               {3}},
+          b = {{0, 1, 2},
+               {3, 4, 5},
+               {6, 7, 8}},
+          c = {{0, 1},
+               {2, 3}},
+          d = {{0, 1}},
+          e = {{0, 1, 2, 3}},
+          f = {{0}},
+          g = {{0, 1, 2}},
+          h = {{0, 1, 2}};
+     vect i = {{a, {{b, {{c},
+                         {d}}},
+                    {e,   f}}},
+               {g,        h}};
+     cout << i << endl;
+     return EXIT_SUCCESS;
 }
